74c043f73fba8e85d76adc1de16938f1dc06e4fe
Nicholas Tate <98060238+Kinny-B@users.noreply.github.com>
2024-03-03 00:16:00 -0600

    changed to fit my fork of example database

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://testUser:TrSKE0QbAOlcm35j@cluster0.0gsqehw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -13,8 +13,7 @@ console.log('Server started at http://localhost:' + port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// routes will go here
-
+  // routes will go here
 // Default route:
 app.get('/', function(req, res) {
   const myquery = req.query;
@@ -35,8 +34,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('myDB');
+    const parts = database.collection('stuff');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
